9fd4033e7409d118e5245e3d1ad660b5d8b709e5
liangshan <loveliangshan@126.com>
2019-10-12 14:13:25 +0800

    no message

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -5,57 +5,76 @@
 </template>
 
 <script>
-import * as types from './store/mutation-types'
-export default {
-  name: 'App',
-  sockets: {
-    disconnect () {
-      this.$store.commit(types.CACHE_CONNECT_STATUS, {
-        connectStatus: false
-      })
-    },
-    reconnect () {
-      this.$store.commit(types.CACHE_CONNECT_STATUS, {
-        connectStatus: true
-      })
-    },
-    hi (data) {
-      console.log('.....', JSON.stringify(data, null, 2))
-      this.$store.commit(types.CACHE_ENKEL_ENV, {
-        enkelEnv: data
-      })
-      this.$store.commit(types.CACHE_CURRENT_PATH, {
-        currentPath: data.PWD
-      })
+  import * as types from './store/mutation-types'
+  export default {
+    name: 'App',
+    sockets: {
+      disconnect () {
+        this.$store.commit(types.CACHE_CONNECT_STATUS, {
+          connectStatus: false
+        })
+      },
+      reconnect () {
+        this.$store.commit(types.CACHE_CONNECT_STATUS, {
+          connectStatus: true
+        })
+      },
+      hi (data) {
+        console.log('.....', JSON.stringify(data, null, 2))
+        this.$store.commit(types.CACHE_ENKEL_ENV, {
+          enkelEnv: data
+        })
+        this.$store.commit(types.CACHE_CURRENT_PATH, {
+          currentPath: data.PWD
+        })
+      }
     }
   }
-}
 </script>
 
 <style lang="less">
-* {
-  margin: 0;
-  padding: 0;
-}
-html,
-body {
-  height: 100%;
-}
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
-  height: 100%;
-}
-// #nav {
-//   padding: 30px;
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  html,
+  body {
+    height: 100%;
+  }
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    // text-align: center;
+    height: 100%;
+  }
+  // #nav {
+  //   padding: 30px;
+  //   a {
+  //     font-weight: bold;
+  //     color: #2c3e50;
+  //     &.router-link-exact-active {
+  //       color: #42b983;
+  //     }
+  //   }
+  // }
+  .generate-form {
+    .ivu-picker-panel-sidebar {
+      padding-top: 10px;
+    }
+    .ivu-input {
+      font-size: 14px;
+    }
+    .ivu-select-selected-value {
+      font-size: 14px !important;
+    }
+  }
+
+  .ivu-tooltip-popper {
+    .ivu-tooltip-inner {
+      max-width: 300px;
+      word-break: break-all;
+      white-space: pre-wrap;
+    }
+  }
 </style>
--- ui/src/views/Create.vue
+++ ui/src/views/Create.vue
@@ -1,29 +1,71 @@
 <template>
   <div class="generate-container">
-    <div class="generate-header">Weekly 周报生成器</div>
+    <div class="generate-header">Weekly</div>
     <div class="generate-content">
       <div class="generate-inner">
         <div class="generate-form">
-          <Form :label-width="80"
+          <Form :label-width="75"
                 :model="generateFormData">
+            <FormItem label="仓库">
+              <Select v-model="activeRepo"
+                      label-in-value
+                      @on-change="changeRepository">
+                <Option value="default">自定义</Option>
+                <Option v-for="(repo, index) in repositories"
+                        :key="index"
+                        :value="repo.url"
+                        :label="repo.name">
+                  <Tooltip :content="repo.url"
+                           placement="bottom"
+                           style="width: 100%;"
+                           transfer>
+                    <div class="option-item">
+                      <span class="option-item-name">{{repo.name}}</span>
+                      <span class="option-item-url">{{repo.url}}</span>
+                    </div>
+                  </Tooltip>
+                </Option>
+              </Select>
+            </FormItem>
             <FormItem label="仓库地址">
               <Input placeholder="请输入仓库地址"
-                     v-model="generateFormData.url" />
+                     :disabled="activeRepo !== 'default'"
+                     v-model="generateFormData.url"
+                     clearable
+                     @on-change="changeRepoUrl" />
             </FormItem>
-            <FormItem label="仓库账号">
-              <Input placeholder="请输入仓库账号"
-                     v-model="generateFormData.username" />
+            <FormItem label="仓库名称">
+              <Input placeholder="请输入仓库名称"
+                     v-model="generateFormData.name"
+                     clearable
+                     :disabled="activeRepo !== 'default'"
+                     @on-change="setInputManually(true)" />
             </FormItem>
-            <FormItem label="仓库密码">
-              <Input placeholder="请输入仓库密码"
-                     v-model="generateFormData.password" />
-            </FormItem>
+            <transition name="fade">
+              <FormItem label="仓库账号"
+                        v-if="activeRepo === 'default'"
+                        key="repo-username">
+                <Input placeholder="请输入仓库账号"
+                       clearable
+                       v-model="generateFormData.username" />
+              </FormItem>
+            </transition>
+            <transition name="fade">
+              <FormItem label="仓库密码"
+                        v-if="activeRepo === 'default'"
+                        key="repo-password">
+                <Input placeholder="请输入仓库密码"
+                       clearable
+                       v-model="generateFormData.password" />
+              </FormItem>
+            </transition>
             <FormItem label="时间">
-              <DatePicker type="daterange"
+              <DatePicker type="datetimerange"
                           :options="reportDateOptions"
                           placement="bottom-end"
                           placeholder="请选择起止时间"
-                          style="width: 100%;"></DatePicker>
+                          style="width: 100%;"
+                          v-model="generateDate"></DatePicker>
             </FormItem>
             <FormItem>
               <Button type="primary"
@@ -42,129 +84,226 @@
 </template>
 
 <script>
-import { Tooltip, Icon, Poptip, Switch, Input, Modal, Button, Form, FormItem, Select, Option, Spin, DatePicker } from 'iview'
-export default {
-  name: 'Create',
-  components: {
-    Tooltip, Icon, Poptip,
-    iSwitch: Switch,
-    Input, Modal, Button, Form, FormItem, Select, Option, Spin, DatePicker
-  },
-  sockets: {
-    'LIST_REPOSITORIES' (data) {
-      this.repositories = data.repositories
-    }
-  },
-  data () {
-    return {
-      repositories: [],
-      isGenerating: false, // 正在生成周报
-      generateFormData: {
-        name: '',
-        url: '',
-        username: '',
-        password: ''
+  import { Tooltip, Icon, Poptip, Switch, Input, Modal, Button, Form, FormItem, Select, Option, Spin, DatePicker } from 'iview'
+  export default {
+    name: 'Create',
+    components: {
+      Tooltip, Icon, Poptip,
+      iSwitch: Switch,
+      Input, Modal, Button, Form, FormItem, Select, Option, Spin, DatePicker
+    },
+    sockets: {
+      'LIST_REPOSITORIES' (data) {
+        this.repositories = data.repositories
+      }
+    },
+    data () {
+      return {
+        repositories: [],
+        isGenerating: false, // 正在生成周报
+        generateDate: [],
+        inputManually: false, // 是否手动输入的仓库名称
+        activeRepo: 'default', // 自定义仓库
+        generateFormData: {
+          name: '',
+          url: '',
+          username: '',
+          password: '',
+          date: []
+        },
+        reportDateOptions: {
+          shortcuts: [
+            {
+              text: '上周',
+              value () {
+                let now = new Date()
+                let start = new Date()
+                let end = new Date()
+                var weekday = now.getDay() || 7 // 获取星期几,getDay()返回值是 0（周日） 到 6（周六） 之间的一个整数。0||7为7，即weekday的值为1-7
+                start.setDate(now.getDate() - weekday - 7 + 1) // 往前算（weekday-1）天，年份、月份会自动变化
+                start.setHours(0)
+                start.setMinutes(0)
+                start.setSeconds(0)
+                end.setTime(start.getTime() + 7 * 24 * 60 * 60 * 1000 - 1000)
+                return [start, end];
+              }
+            },
+            {
+              text: '本周',
+              value () {
+                let now = new Date()
+                let start = new Date()
+                let end = new Date()
+                var weekday = now.getDay() || 7 // 获取星期几,getDay()返回值是 0（周日） 到 6（周六） 之间的一个整数。0||7为7，即weekday的值为1-7
+                start.setDate(now.getDate() - weekday + 1) // 往前算（weekday-1）天，年份、月份会自动变化
+                start.setHours(0)
+                start.setMinutes(0)
+                start.setSeconds(0)
+                end.setTime(start.getTime() + 7 * 24 * 60 * 60 * 1000 - 1000)
+                return [start, end];
+              }
+            },
+            {
+              text: '本月',
+              value () {
+                let now = new Date()
+                let year = now.getFullYear()
+                let month = now.getMonth() + 1
+                let start = new Date(year + '-' + month + '-1 0:0:0')
+                let nextMonth
+                let nextYear = year
+                if (month < 12) {
+                  nextMonth = month + 1
+                } else {
+                  nextMonth = 1
+                  nextYear += 1
+                }
+                let end = new Date(nextYear + '-' + nextMonth + '-1 0:0:0')
+                end.setTime(end.getTime() - 1000)
+                return [start, end]
+              }
+            },
+            {
+              text: '最近3个月',
+              value () {
+                const end = new Date();
+                const start = new Date();
+                start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+                return [start, end];
+              }
+            }
+          ]
+        }
+      }
+    },
+    created () {
+      this.listRepositories()
+    },
+    methods: {
+      getThisWeekDate () {
+        let now = new Date()
+        let start = new Date()
+        let end = new Date()
+        var weekday = now.getDay() || 7 // 获取星期几,getDay()返回值是 0（周日） 到 6（周六） 之间的一个整数。0||7为7，即weekday的值为1-7
+        start.setDate(now.getDate() - weekday + 1) // 往前算（weekday-1）天，年份、月份会自动变化
+        start.setHours(0)
+        start.setMinutes(0)
+        start.setSeconds(0)
+        end.setTime(start.getTime() + 7 * 24 * 60 * 60 * 1000 - 1000)
+        return [start, end];
       },
-      reportDateOptions: {
-        shortcuts: [
-          {
-            text: '本周',
-            value () {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-              return [start, end];
-            }
-          },
-          {
-            text: '本月',
-            value () {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
-              return [start, end];
-            }
-          },
-          {
-            text: '最近3个月',
-            value () {
-              const end = new Date();
-              const start = new Date();
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
-              return [start, end];
-            }
-          }
-        ]
+      listRepositories () {
+        this.$exec({
+          action: 'list-repositories'
+        })
+      },
+      setInputManually (flag) {
+        this.inputManually = flag
+      },
+      changeRepoUrl () {
+        if (!this.inputManually) {
+          // 用户没手动输入过仓库名称，则自动生成
+          this.generateFormData.name = this.generateFormData.url.split('/').pop()
+        }
+      },
+      changeRepository (repo) {
+        if (repo.value !== 'default') {
+          this.generateFormData.name = repo.label
+          this.generateFormData.url = repo.value
+        } else {
+          this.generateFormData.name = ''
+          this.generateFormData.url = ''
+        }
+      },
+      generate () {
+        // 生成周报
+        this.generateFormData.date = this.generateDate.map(item => item.getTime())
+        this.isGenerating = true
+        setTimeout(() => {
+          this.isGenerating = false
+        }, 3000)
+        // this.$exec({
+        //   action: 'generate',
+        //   data: this.generateFormData
+        // })
+        this.setInputManually(false)
       }
     }
-  },
-  created () {
-    this.listRepositories()
-  },
-  methods: {
-    listRepositories () {
-      this.$exec({
-        action: 'list-repositories'
-      })
-    },
-    generate () {
-      // 生成周报
-      this.$exec({
-        action: 'generate',
-        data: this.generateFormData
-      })
-    }
   }
-}
 </script>
 
 <style lang="less" scoped>
-@import url("../themes/index.less");
-.generate-container {
-  width: 100%;
-  height: 100%;
-  .generate-header {
+  @import url("../themes/index.less");
+  .generate-container {
     width: 100%;
-    height: 64px;
-    padding: 10px 0;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: row;
-    align-items: flex-end;
-    justify-content: center;
-    color: #ffffff;
-    background-color: @primary-color;
-    font-size: 18px;
-    user-select: none;
-  }
-  .generate-content {
-    width: 100%;
-    height: calc(100% - 64px);
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    .generate-inner {
-      width: 1000px;
-      height: 100%;
+    height: 100%;
+    .generate-header {
+      width: 100%;
+      height: 64px;
+      padding: 10px 0;
+      box-sizing: border-box;
       display: flex;
       flex-direction: row;
-      align-items: flex-start;
-      justify-content: space-between;
-      .generate-form {
-        width: 400px;
+      align-items: flex-end;
+      justify-content: center;
+      color: #ffffff;
+      background-color: @primary-color;
+      font-size: 18px;
+      user-select: none;
+    }
+    .generate-content {
+      width: 100%;
+      height: calc(100% - 64px);
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      .generate-inner {
+        width: 1000px;
         height: 100%;
-        padding: 30px 10px 30px 0;
-        box-sizing: border-box;
-      }
-      .generate-result {
-        width: 600px;
-        height: 100%;
-        padding: 30px 0 30px 10px;
-        box-sizing: border-box;
-        background-color: goldenrod;
+        display: flex;
+        flex-direction: row;
+        align-items: flex-start;
+        justify-content: space-between;
+        .generate-form {
+          width: 400px;
+          height: 100%;
+          padding: 30px 10px 30px 0;
+          box-sizing: border-box;
+          input {
+            font-size: 18px !important;
+          }
+          .option-item {
+            width: 100%;
+            font-size: 14px;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: space-between;
+            .option-item-name {
+              flex: 1;
+              padding-right: 15px;
+              font-weight: bold;
+            }
+            .option-item-url {
+              max-width: 400px;
+              overflow: hidden;
+              text-overflow: ellipsis;
+              white-space: nowrap;
+              color: #bbb;
+              display: inline-block;
+              vertical-align: middle;
+            }
+          }
+        }
+        .generate-result {
+          width: 600px;
+          height: 100%;
+          padding: 30px 0 30px 10px;
+          box-sizing: border-box;
+          background-color: lightcyan;
+        }
       }
     }
   }
-}
 </style>
